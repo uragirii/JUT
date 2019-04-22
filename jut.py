efbe35a9aaba0ca31bfe8c1641aeed27fc385532
Cherub <apoorvkansalak@gmail.com>
2019-04-23 02:14:41 +0530

    Added the add new comment and tag subcommand

--- jut.py
+++ jut.py
@@ -3,13 +3,12 @@ import json
 import os
 import datetime
 
+
 class Project:
 
-    def __init__(self, init_called = False):
+    def __init__(self, init_called=False):
         # check whether the file exits or not
-        if init_called:
-            click.echo("Init called")
-        elif os.path.exists('jut.json'):
+        if os.path.exists('jut.json'):
             # Read the data from json file.
             with open('jut.json') as proj_file:
                 data = json.load(proj_file)
@@ -20,11 +19,14 @@ class Project:
                 self.tags = data['tags']
                 self.comments = data['comments']
                 self.desc = data['desc']
-                self.start_date = data['startDate']
+                self.startDate = data['startDate']
+        elif init_called:
+            pass
         else:
             # This ensures that whenever command runs, the `jut.json` file exits.
             click.secho("\nThe project file does not exists, run  `jut init` command to create new project file"
                         "\nClosing.", fg="red")
+            raise click.Abort
 
     def __str__(self):
         return self.name
@@ -51,12 +53,12 @@ class Project:
                         break
                     else:
                         taglist.append(new_tag.upper())
-            self.tags ={}
+            self.tags = {}
             for tag in taglist:
                 # initialize every tag as empty
                 self.tags[tag] = []
-            self.start_date = datetime.datetime.now().replace(microsecond=0).isoformat()
-            click.echo("\nStart Date: {0}".format(self.start_date))
+            self.startDate = datetime.datetime.now().replace(microsecond=0).isoformat()
+            click.echo("\nStart Date: {0}".format(self.startDate))
             self.comments = []
             self.jver = 'K'
             # now dump to json file.
@@ -65,21 +67,53 @@ class Project:
             # todo: after start a git prompt.
 
     def _dump_json(self):
-        with open('jut.json','w') as jfile:
-            json.dump(self.__dict__,jfile)
+        with open('jut.json', 'w') as jfile:
+            json.dump(self.__dict__, jfile)
+
+    def add_comment(self, comment):
+        # Check is comment is None.
+        if comment is None:
+            return None
+
+        comment_data = (str(datetime.datetime.now().replace(microsecond=0).isoformat()), comment)
+        self.comments.insert(0, comment_data)
+        # Now write data again.
+        self._dump_json()
+        click.secho("Successfully added comment {} at time {}".format(comment, comment_data[0]), fg="green")
+
+    def add_tag(self, tag):
+        # Check if tag is none, meaning value is not passed.
+        if tag is None:
+            return None
+
+        tag_name, tag_msg = tag
+
+        if not tag_name.upper() in self.tags.keys():
+            # check is tag name does not exists
+            choice = click.confirm("Tag Name you have entered could not be found, do you want to add the tag name?")
+
+            if not choice:
+                raise click.Abort
+
+        tag_data = (str(datetime.datetime.now().replace(microsecond=0).isoformat()), tag_msg)
+        self.tags[tag_name.upper()] = tag_data
+        click.secho("Successfully added '{}' to tag '{}' at time {}".format(tag_msg, tag_name.upper(), tag_data[0]),
+                    fg="green")
+        self._dump_json()
+
+    def print_proj(self):
+        print(self.__dict__)
 
 
 @click.group(invoke_without_command = True)
 def cli():
-    click.clear()
+    # click.clear()
     click.secho("="*25, fg="green")
     click.secho("\nThis is development version.Please bear for any mistakes", fg="red")
     click.secho("\nIf you find any error or bug please mail me at \n\t mldata.apoorv@gmail.com\n", fg="red")
     click.secho("="*25, fg="green")
     click.pause("(Press any key to continue)")
-    click.clear()
-    # todo: !IMP call the instance of project and check whether file exists or not
-    # project = Project()
+    # click.clear()
 
 
 @cli.command()
@@ -107,8 +141,21 @@ def show(tag):
 
 
 @cli.command()
-def add():
+@click.option('-c', '--comment', nargs=1)
+@click.option('-t', '--tag', nargs=2)
+# @click.option('-tag', '--t', default="", nargs=2)
+def add(comment, tag):
     """
     Adds new comments or tagged comment
     """
-    pass
+    new_proj = Project()
+    # The initialization of class checks whether the file is present or not
+    # todo: implement a faster way to write to json file. rn I'm just reading and "writing" not altering the file.
+    if comment is None and tag is None:
+        raise click.Abort
+    else:
+        # If comment is None, it will be checked by the class.
+        new_proj.add_comment(comment)
+        if all(tag):
+            new_proj.add_tag(tag)
+
